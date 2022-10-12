74485bfeb8f66ded0d290d2d2e289b4e6bd19827
farhatdk <farhatdk98@gmail.com>
2022-10-12 15:53:17 +0700

    meta nostalgia description added

--- src/components/common/Helmet.tsx
+++ src/components/common/Helmet.tsx
@@ -10,16 +10,16 @@ export default function Helmet(props: HelmetProps) {
     <>
       <Head>
         <title>Kamus Nostalgia - Translator Bahasa G | {pageTitle}</title>
-        <meta name="description" content="Kamnos by Koneksi Group" />
+        <meta name="description" content="Nostalgia bersama bahasa G yang sempet booming yuk!" />
         <meta name="image" content="https://i.ibb.co/M7Ncnpr/logo-kamus.png" />
         <meta property="og:url" content="https://kamus-nostalgia.vercel.app" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Kamus Nostalgia - Translator Bahasa G | Kamus Nostalgia"/>
-        <meta property="og:description" content="Kamnos by Koneksi Group" />
+        <meta property="og:title" content={`Kamus Nostalgia - Translator Bahasa G | ${pageTitle}`}/>
+        <meta property="og:description" content="Nostalgia bersama bahasa G yang sempet booming yuk!" />
         <meta property="og:image" content="https://i.ibb.co/M7Ncnpr/logo-kamus.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Kamus Nostalgia - Translator Bahasa G | Kamus Nostalgia"/>
-        <meta name="twitter:description" content="Kamnos by Koneksi Group" />
+        <meta name="twitter:title" content={`Kamus Nostalgia - Translator Bahasa G | ${pageTitle}`}/>
+        <meta name="twitter:description" content="Nostalgia bersama bahasa G yang sempet booming yuk!" />
         <meta name="twitter:image" content="https://i.ibb.co/M7Ncnpr/logo-kamus.png"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
