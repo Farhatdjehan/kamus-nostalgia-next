ef296afbc247d19ad61a245fa6b4fe7f14dcd963
farhatdk <farhatdk98@gmail.com>
2022-10-12 15:46:25 +0700

    link image content

--- src/components/common/Helmet.tsx
+++ src/components/common/Helmet.tsx
@@ -11,16 +11,16 @@ export default function Helmet(props: HelmetProps) {
       <Head>
         <title>Kamus Nostalgia - Translator Bahasa G | {pageTitle}</title>
         <meta name="description" content="Kamnos by Koneksi Group" />
-        <meta name="image" content="https://ibb.co/zZfXsbp" />
+        <meta name="image" content="https://i.ibb.co/M7Ncnpr/logo-kamus.png" />
         <meta property="og:url" content="https://kamus-nostalgia.vercel.app" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Kamus Nostalgia - Translator Bahasa G | Kamus Nostalgia"/>
         <meta property="og:description" content="Kamnos by Koneksi Group" />
-        <meta property="og:image" content="https://ibb.co/zZfXsbp" />
+        <meta property="og:image" content="https://i.ibb.co/M7Ncnpr/logo-kamus.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Kamus Nostalgia - Translator Bahasa G | Kamus Nostalgia"/>
         <meta name="twitter:description" content="Kamnos by Koneksi Group" />
-        <meta name="twitter:image" content="https://ibb.co/zZfXsbp"></meta>
+        <meta name="twitter:image" content="https://i.ibb.co/M7Ncnpr/logo-kamus.png"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
